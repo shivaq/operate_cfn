42f836debbfc776d80d856f6c784592ed9d57e4f
Yasuaki Shibata <shivaq777@gmail.com>
2019-12-01 23:32:42 +0900

    Set todo

--- set_mfa.py
+++ set_mfa.py
@@ -1,7 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import boto3
 
+# TODO: set custom config file
 
-ec2_client = boto3.client('ec2')
-print("Hello!")
+# TODO: Check existence of custom config file
+
+# TODO: Get an mfa arn from custom config file
+
+# TODO: get a config file from ~/.aws/config
+
+# TODO: Get profile from an argument
+
+# TODO: Get an MFA token from an argument
+
+# TODO: Get a role to switch from an argument
+
+# TODO: Use default profile and role only specify mfa code
+
+# TODO: Check if the profile exists
+
+# TODO: Check if the mfa token is int
+
+# TODO: Check if the mfa is longer than 6
+
+# TODO: get session token
+
+# TODO: Set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN, AWS_SECURITY_TOKEN on environment variables
+
+# TODO: Set AWS_SDK_LOAD_CONFIG=true
+
+# TODO: Set AWS_PROFILE
+
+# TODO: AWS_DEFAULT_REGION
