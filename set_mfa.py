d0fe8537d53bb27c8576fa8cd8c70068c1db2d75
Yasuaki Shibata <shivaq777@gmail.com>
2019-12-06 07:46:52 +0900

    Create get profile function

--- set_mfa.py
+++ set_mfa.py
@@ -27,9 +27,26 @@ logger.addHandler(ch)
 logger.addHandler(rfh)
 logger.propagate = False
 
-# TODO: set custom config file
+##################
+# configure config file
+##################
+Config = configparser.ConfigParser()
+Config._interpolation = configparser.ExtendedInterpolation()
 
-# TODO: Check existence of custom config file
+
+def get_cfg():
+    """Check custom config files"""
+    logger.debug("get_cfg is called")
+
+    home = Path.home()
+    custom_cfg_path = home / 'operate_cfn' / 'aws.ini'
+
+    if not custom_cfg_path.exists():
+        logger.error('{0} is not exist. Please create it.'.format(custom_cfg_path))
+        sys.exit(1)
+
+    return custom_cfg_path
+
 
 # TODO: Get an mfa arn from custom config file
 
@@ -58,3 +75,48 @@ logger.propagate = False
 # TODO: Set AWS_PROFILE
 
 # TODO: AWS_DEFAULT_REGION
+
+# TODO: Check Win or posix
+
+
+def select_profile():
+    """provide profile selection to user input"""
+
+    sections = Config.sections()
+    str_profile = "profile"
+    profiles = []
+
+    # extract profile from sections
+    for section in sections:
+        logger.debug(section)
+        if section.startswith(str_profile):
+            profiles.append(section)
+
+    print("Selectable profiles")
+    # present profile number and name
+    for profile in profiles:
+        print(str(profiles.index(profile) + 1) + ") " + profile[len(str_profile) + 1:])
+
+    while True:
+        profile_num = input("Input the number of the profile and ENTER.  ")
+        if 1 <= int(profile_num) <= len(profiles):
+            print("Thank you.")
+            break
+        else:
+            print("wrong number")
+
+    selected_profile = profiles[int(profile_num) - 1][len(str_profile) + 1:]
+    print("You selected {}.".format(selected_profile))
+    return selected_profile
+
+
+# get a config file
+Config.read(get_cfg())
+
+select_profile()
+
+# print("Print options: {}".format(Config.options("default")))
+# print("Print value: {}".format(Config.get("production", "test")))
+# print("Print value: {}".format(Config.get("SectionThree", "Charlie")))
+
+# input("Which account do you use?")
